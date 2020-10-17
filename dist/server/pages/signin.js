6e04e1ddf9dfbc14950f4d0b32af6e63afedb289
Andrew Lovett-Barron <alb@andrewlb.com>
2020-10-17 23:04:02 +0200

    Update to nextauth

--- dist/server/pages/signin.js
+++ dist/server/pages/signin.js
@@ -23,9 +23,9 @@ var _default = (_ref) => {
     error
   } = req.query;
   var providersToRender = providers.filter(provider => {
-    if (provider.type === 'oauth' || provider.type === 'email') {
+    if (provider.type === "oauth" || provider.type === "email") {
       return true;
-    } else if (provider.type === 'credentials' && provider.credentials) {
+    } else if (provider.type === "credentials" && provider.credentials) {
       return true;
     } else {
       return false;
@@ -35,24 +35,24 @@ var _default = (_ref) => {
 
   if (error) {
     switch (error) {
-      case 'Signin':
-      case 'OAuthSignin':
-      case 'OAuthCallback':
-      case 'OAuthCreateAccount':
-      case 'EmailCreateAccount':
-      case 'Callback':
+      case "Signin":
+      case "OAuthSignin":
+      case "OAuthCallback":
+      case "OAuthCreateAccount":
+      case "EmailCreateAccount":
+      case "Callback":
         errorMessage = (0, _preact.h)("p", null, "Try signing with a different account.");
         break;
 
-      case 'OAuthAccountNotLinked':
+      case "OAuthAccountNotLinked":
         errorMessage = (0, _preact.h)("p", null, "To confirm your identity, sign in with the same account you used originally.");
         break;
 
-      case 'EmailSignin':
+      case "EmailSignin":
         errorMessage = (0, _preact.h)("p", null, "Check your email address.");
         break;
 
-      case 'CredentialsSignin':
+      case "CredentialsSignin":
         errorMessage = (0, _preact.h)("p", null, "Sign in failed. Check the details you provided are correct.");
         break;
 
@@ -69,10 +69,10 @@ var _default = (_ref) => {
   }, errorMessage), providersToRender.map((provider, i) => (0, _preact.h)("div", {
     key: provider.id,
     className: "provider"
-  }, provider.type === 'oauth' && (0, _preact.h)("form", {
+  }, provider.type === "oauth" && (0, _preact.h)("form", {
     action: provider.signinUrl,
     method: "POST"
-  }, (0, _preact.h)("input", {
+  }, provider.preface && (0, _preact.h)("p", null, provider.preface), (0, _preact.h)("input", {
     type: "hidden",
     name: "csrfToken",
     value: csrfToken
@@ -83,7 +83,7 @@ var _default = (_ref) => {
   }), (0, _preact.h)("button", {
     type: "submit",
     className: "button"
-  }, "Sign in with ", provider.name)), (provider.type === 'email' || provider.type === 'credentials') && i > 0 && providersToRender[i - 1].type !== 'email' && providersToRender[i - 1].type !== 'credentials' && (0, _preact.h)("hr", null), provider.type === 'email' && (0, _preact.h)("form", {
+  }, "Sign in with ", provider.name)), (provider.type === "email" || provider.type === "credentials") && i > 0 && providersToRender[i - 1].type !== "email" && providersToRender[i - 1].type !== "credentials" && (0, _preact.h)("hr", null), provider.type === "email" && (0, _preact.h)("form", {
     action: provider.signinUrl,
     method: "POST"
   }, (0, _preact.h)("input", {
@@ -101,7 +101,7 @@ var _default = (_ref) => {
     placeholder: "email@example.com"
   }), (0, _preact.h)("button", {
     type: "submit"
-  }, "Sign in with ", provider.name)), provider.type === 'credentials' && (0, _preact.h)("form", {
+  }, "Sign in with ", provider.name)), provider.type === "credentials" && (0, _preact.h)("form", {
     action: provider.callbackUrl,
     method: "POST"
   }, (0, _preact.h)("input", {
@@ -116,13 +116,13 @@ var _default = (_ref) => {
     }, provider.credentials[credential].label || credential), (0, _preact.h)("input", {
       name: credential,
       id: "input-".concat(credential, "-for-").concat(provider.id, "-provider"),
-      type: provider.credentials[credential].type || 'text',
-      value: provider.credentials[credential].value || '',
-      placeholder: provider.credentials[credential].placeholder || ''
+      type: provider.credentials[credential].type || "text",
+      value: provider.credentials[credential].value || "",
+      placeholder: provider.credentials[credential].placeholder || ""
     }));
   }), (0, _preact.h)("button", {
     type: "submit"
-  }, "Sign in with ", provider.name)), (provider.type === 'email' || provider.type === 'credentials') && i + 1 < providersToRender.length && (0, _preact.h)("hr", null)))));
+  }, "Sign in with ", provider.name)), (provider.type === "email" || provider.type === "credentials") && i + 1 < providersToRender.length && (0, _preact.h)("hr", null)))));
 };
 
 exports.default = _default;
